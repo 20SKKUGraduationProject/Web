502ba0b7fc4963d92aed3189e6a758b5db6d90f7
joona95 <joona95@naver.com>
2020-04-27 11:05:07 +0900

    Create TimeTable model

--- backend/createtimetable/models.py
+++ backend/createtimetable/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class TimeTable(models.Model):
+    studentID = models.CharField(max_length=200)
+    studentName = models.CharField(max_length=200)
+    courses = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.studentID
+
